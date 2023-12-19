dd7977f6129d277d6d218e42fc2568b09d171098
Eric Williams <ewilliams0305@gmail.com>
2023-12-19 17:53:21 -0500

    created all required flags to complete the operations

--- pkg/tui/flags.go
+++ pkg/tui/flags.go
@@ -10,27 +10,38 @@ var (
 	Hostname string
 	// The API token generated from the VC4 webpage, this is required to control an external appliance
 	Token string
-
-	// The room id passed into the application. providing a room id creates additional options that can be leveraged to process room actions
-	RoomID string
 	// Program file path passed into the application used to create a new program entry
 	ProgramFile string
+	// The programs and room name used to whip up a new room with a simply command line arg
+	ProgramName string
+	// The room id passed into the application. providing a room id creates additional options that can be leveraged to process room actions
+	RoomID string
 	// Actions include GET PUT DEL and can be used with a combination of the Room ID and program file flags
 	Action string
 )
 
 func InitFlags() {
 	const (
-		defaultHost  = "127.0.0.1"
-		defaultToken = ""
-		hostUsage    = "The IP or hostname of the virtual control service"
-		tokenUsage   = "The API token generated from the VC4 webpage, this is required to control an external appliance"
+		defaultHost    = "127.0.0.1"
+		defaultToken   = ""
+		hostFlagUsage  = "The IP or hostname of the virtual control service"
+		tokenFlagUsage = "The API token generated from the VC4 webpage, this is required to control an external appliance"
+		progFlagUsage  = "An optional flag to load a program file"
+		nameFagUsage   = "An optional glag used to name the loaded program flag"
+		roomFlagUsage  = "An optional room ID used to spin up a new room"
 	)
-	flag.StringVar(&Hostname, "host", defaultHost, hostUsage)
-	flag.StringVar(&Hostname, "h", defaultHost, hostUsage+" (shorthand)")
-	flag.StringVar(&Token, "token", defaultToken, tokenUsage)
-	flag.StringVar(&Token, "t", defaultToken, tokenUsage+" (shorthand)")
 
-	flag.StringVar(&RoomID, "room", "", "An optional room id used from CRUD operations")
-	flag.StringVar(&RoomID, "r", "", " An optional room id used for CRUD operations (shorthand)")
+	flag.StringVar(&Hostname, "host", defaultHost, hostFlagUsage)
+	flag.StringVar(&Hostname, "h", defaultHost, hostFlagUsage+" (shorthand)")
+	flag.StringVar(&Token, "token", defaultToken, tokenFlagUsage)
+	flag.StringVar(&Token, "t", defaultToken, tokenFlagUsage+" (shorthand)")
+
+	flag.StringVar(&RoomID, "room", "", roomFlagUsage)
+	flag.StringVar(&RoomID, "r", "", roomFlagUsage+" (shorthand)")
+
+	flag.StringVar(&ProgramFile, "program", "", progFlagUsage)
+	flag.StringVar(&ProgramFile, "p", "", progFlagUsage+" (shorthand)")
+
+	flag.StringVar(&ProgramName, "name", "", nameFagUsage)
+	flag.StringVar(&ProgramName, "n", "", nameFagUsage+" (shorthand)")
 }
