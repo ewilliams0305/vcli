727e5e94b9790f5c990047cb0e1345cefb1af332
Eric Williams <ewilliams0305@gmail.com>
2023-12-29 22:08:16 -0500

    Create banner.go

---
+++ pkg/tui/banner.go
@@ -0,0 +1,61 @@
+package tui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type BannerModel struct {
+	message string
+	state   BannerState
+	width   int
+}
+
+type BannerState int
+
+func (m BannerModel) Init() tea.Cmd {
+	return nil
+}
+
+func NewBanner(message string, state BannerState, width int) *BannerModel {
+	return &BannerModel{
+		message: message,
+		state:   state,
+		width:   width,
+	}
+}
+
+func (m BannerModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case BannerState:
+		m.state = msg
+	case string:
+		m.message = msg
+	}
+
+	return m, nil
+}
+
+func (m BannerModel) View() string {
+	return renderBanner(&m)
+}
+
+func renderBanner(model *BannerModel) string {
+	var bg lipgloss.Color
+	if model.state == 0 {
+		bg = lipgloss.Color(PrimaryDark)
+	} else {
+		bg = lipgloss.Color("#FF0000")
+	}
+
+	return lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color(PrimaryLight)).
+		Background(lipgloss.Color(bg)).
+		PaddingTop(1).
+		PaddingLeft(1).
+		MarginBottom(1).
+		Width(model.width).
+		Align(lipgloss.Center).
+		Render(model.message) + "\n\n"
+}
